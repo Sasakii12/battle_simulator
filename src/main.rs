02cdde2f311e46b5ec1a1f0f8c5cceff59f35091
Vipershroom <undertalefan812@gmail.com>
2026-06-09 22:05:06 -0400

    testing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod pokemon;
-use pokemon::{PokemonBaseStats, IVSpread};
+use pokemon::{PokemonBaseStats, IVSpread, EVSpread};
 
 fn main() {
     let meow_base = PokemonBaseStats  {
@@ -13,5 +13,8 @@ fn main() {
         speed: 123,
     };
     let meow_iv = IVSpread { hp: 31, attack: 31, def: 31, spatk: 31, spdef:31, speed: 31};
+    let meow_ev = EVSpread { hp: 0, attack: 0, def: 0, spatk: 0, spdef: 0, speed: 0};
+    let meow = meow_base.evaluate(meow_iv, meow_ev);
     println!("Hello, world!");
+    println!("{}", meow.base_stats.def);
 }
