fc7a58a70d7e21ecd184da02c1a33f80dff7f8de
Vipershroom <undertalefan812@gmail.com>
2026-06-10 13:58:19 -0400

    Add a move

--- src/moves.rs
+++ src/moves.rs
@@ -1,15 +1,30 @@
-use crate::types::Type;
+use crate::types::*;
 
 enum Category {
     Physical,
     Special,
 }
 
+enum Accuracy {
+    Acc(u32),
+    Full,
+}
+
 struct Move {
     move_type: Type<'static>,
     base_power: u32,
     category: Category,
-    accuracy: u32,
+    accuracy: Accuracy,
     priority: u32,
     pp: u32,
 }
+
+
+const FLOWER_TRICK: Move = Move {
+    move_type: GRASS,
+    base_power: 70,
+    category: Category::Physical,
+    accuracy: Accuracy::Full,
+    priority: 0,
+    pp: 10,
+};
