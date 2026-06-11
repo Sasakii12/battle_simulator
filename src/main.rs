47a5276b7e8c4a30df1000d3c8e5207cfbbace6f
Vipershroom <undertalefan812@gmail.com>
2026-06-11 17:14:31 -0400

    Testing damage calc

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ mod types;
 mod moves;
 mod damage;
 use pokemon::{PokemonBaseStats, IVSpread, EVSpread};
-use types::{TypeName, Type, GRASS, DARK};
+use types::{TypeName, Type, GRASS, DARK, NORMAL, FIRE};
+use moves::*;
+use damage::*;
 
 fn main() {
     let meow_base = PokemonBaseStats  {
@@ -16,9 +18,25 @@ fn main() {
         spdef: 70,
         speed: 123,
     };
+    let cinder_base = PokemonBaseStats  {
+        name: String::from("Cinderace"),
+        types: FIRE.eval_type(&NORMAL),
+        hp: 80,
+        attack: 116,
+        def: 75,
+        spatk: 65,
+        spdef: 75,
+        speed: 119,
+    };
     let meow_iv = IVSpread { hp: 31, attack: 31, def: 31, spatk: 31, spdef:31, speed: 31};
     let meow_ev = EVSpread { hp: 0, attack: 0, def: 0, spatk: 0, spdef: 0, speed: 0};
     let meow = meow_base.evaluate(meow_iv, meow_ev);
+
+    let cinder_iv = IVSpread { hp: 31, attack: 31, def: 31, spatk: 31, spdef:31, speed: 31};
+    let cinder_ev = EVSpread { hp: 0, attack: 0, def: 0, spatk: 0, spdef: 0, speed: 0};
+    let cinder = cinder_base.evaluate(cinder_iv, cinder_ev);
     println!("Hello, world!");
     println!("{:?}", meow.base_stats.types);
+    println!("{:?}", cinder.base_stats.types);
+    println!("{}", damage::damage(meow, cinder, moves::FLOWER_TRICK));
 }
